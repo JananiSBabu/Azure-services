37b0afadccf429cdc9b61047405506edca957c26
JananiSBabu <62363691+JananiSBabu@users.noreply.github.com>
2022-06-02 17:04:22 -0700

    added custom events -  webhook api subscription incomplete

---
+++ Events/CustomEvents/ConsoleAppPublisher/ConsoleAppPublisher/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Xml;
+using Azure;
+using Azure.Messaging.EventGrid;
+
+namespace ConsoleAppPublisher
+{
+    class Program
+    {
+        private const string topicEndpoint = "https://accountcreated.westus-1.eventgrid.azure.net/api/events";
+        private const string topicKey = "VjRO7dnpP7BviNBVUtOpndlWZlxN13mNjnPc2gPkWkg=";
+
+        private static readonly Uri uriEndpoint = new Uri(topicEndpoint);
+
+
+        static async Task Main(string[] args)
+        {
+            // authenticate to event grid -> azure key credential
+            AzureKeyCredential credential = new AzureKeyCredential(topicKey);
+
+            EventGridPublisherClient client = new EventGridPublisherClient(uriEndpoint, credential);
+
+            EventGridEvent acct = new EventGridEvent(
+                subject: "New account",
+                eventType: "New account created",
+                dataVersion: "1.0",
+                data: new { Message = "Hi" });
+
+            await client.SendEventAsync(acct);
+            Console.WriteLine("Event published");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Events/CustomEvents/EventHandlerAPI/EventHandlerAPI/Controllers/CustomEventsController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Threading.Tasks;
+using Azure.Messaging.EventGrid;
+using Azure.Messaging.EventGrid.SystemEvents;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace EventHandlerAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CustomEventsController : ControllerBase
+    {
+        private readonly ILogger<CustomEventsController> _logger;
+
+        public CustomEventsController(ILogger<CustomEventsController> logger)
+        {
+            _logger = logger;
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            string response = string.Empty;
+            BinaryData events = await BinaryData.FromStreamAsync(Request.Body);
+
+
+            EventGridEvent[] eventGridEvents = EventGridEvent.ParseMany(events);
+
+            foreach (EventGridEvent eventGridEvent in eventGridEvents)
+            {
+                // Handle system events
+                if (eventGridEvent.TryGetSystemEventData(out object eventData))
+                {
+                    // Handle the subscription validation event
+                    if (eventData is SubscriptionValidationEventData subscriptionValidationEventData)
+                    {
+                        // Log.LogInformation($"Got SubscriptionValidation event data, validation code: {subscriptionValidationEventData.ValidationCode}, topic: {eventGridEvent.Topic}");
+                        // Do any additional validation (as required) and then return back the below response
+
+                        var responseData = new SubscriptionValidationResponse()
+                        {
+                            ValidationResponse = subscriptionValidationEventData.ValidationCode
+                        };
+                        return new OkObjectResult(responseData);
+                    }
+                }
+            }
+            return new OkObjectResult(response);
+
+        }
+    }
+}
