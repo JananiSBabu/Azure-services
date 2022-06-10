6e471ac2ce79b128407c5ec79b3edae00124cb86
JananiSBabu <62363691+JananiSBabu@users.noreply.github.com>
2022-06-10 09:48:52 -0700

    Function app for Blob trigger

---
+++ Function app/FunctionAppBlobTrigger/FunctionAppBlobTrigger/FunctionAppBlobTrigger/Function1.cs
@@ -0,0 +1,18 @@
+using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionAppBlobTrigger
+{
+    public static class Function1
+    {
+        [FunctionName("TriggerWhenFileUploads")]
+        public static void Run([BlobTrigger("testblobcontainer1/{name}",
+            Connection = "AzureWebJobsStorage")]Stream myBlob, string name, ILogger log)
+        {
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+        }
+    }
+}
